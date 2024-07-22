9accb5de741a8f81a6b88cc5349dd8f033ada9e9
Qinxin Zhang <44661616+qinxinzhang@users.noreply.github.com>
2024-07-22 22:40:27 +0800

    commit 2

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,21 +1,15 @@
 package com.example.test
 
 import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
-import android.graphics.Rect
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.animation.addListener
-import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 
 
@@ -25,48 +19,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val view = findViewById<View>(R.id.cl_anim)
         val semiCircle = findViewById<ConstraintLayout>(R.id.cl_semi_circle)
-//        val ivCircle = findViewById<ImageView>(R.id.iv_circle)
+        val ivCircle = view.findViewById<ImageView>(R.id.iv_1)
 
         val ivClose = view.findViewById<ImageView>(R.id.iv_close)
 
 
         val ivArrow = semiCircle.findViewById<ImageView>(R.id.iv_arrow)
 
-        val animShow: Animation = AnimationUtils.loadAnimation(this, R.anim.dialog_in)
-        val animHide: Animation = AnimationUtils.loadAnimation(this, R.anim.dialog_out)
 
         val arrowAnimation = TranslateAnimation(0f, 10f, 0f, 0f)
         arrowAnimation.duration = 500
-        arrowAnimation.repeatCount = 6
+        arrowAnimation.repeatCount = 4
         arrowAnimation.repeatMode = Animation.REVERSE
 
-        val displayMetrics = DisplayMetrics()
-        windowManager.defaultDisplay.getMetrics(displayMetrics)
-        val screenHeight = displayMetrics.heightPixels
-        val screenWidth = displayMetrics.widthPixels
-
         val startPos = resources.getDimension(R.dimen.start_pos)
+        val halfCircle = resources.getDimension(R.dimen.move_x_half)
         val width = resources.getDimension(R.dimen.view_width)
         val translation = resources.getDimension(R.dimen.move_x)
 
-
-//        view.setOnClickListener {
-//            val propertyX = PropertyValuesHolder.ofFloat(View.X, 0f)
-//            val propertyY = PropertyValuesHolder.ofFloat(View.Y, 0f)
-//            ObjectAnimator.ofPropertyValuesHolder(view, propertyX, propertyY).apply {
-//                duration = 2000L
-//                start()
-//            }
-//        }
         val expand = ObjectAnimator.ofFloat(view, "translationX", 0f)
         val fold = ObjectAnimator.ofFloat(view, "translationX", startPos)
+
+        val expandHalfCircle = ObjectAnimator.ofFloat(view, "translationX", halfCircle)
+        expand.duration = 500
+        fold.duration = 500
+        expandHalfCircle.duration = 200
+
+        val scaleX = ObjectAnimator.ofFloat(ivCircle, "scaleX", 0f, 1f)
+        val scaleY = ObjectAnimator.ofFloat(ivCircle, "scaleY", 0f, 1f)
+
+        scaleX.setDuration(200)
+        scaleY.setDuration(200)
+
+
+        view.setOnClickListener {
+            expand.start()
+        }
+
+        ivClose.setOnClickListener {
+            fold.start()
+        }
         fold.addListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(p0: Animator) {
 
             }
 
             override fun onAnimationEnd(p0: Animator) {
-
+                ivArrow.startAnimation(arrowAnimation)
+                semiCircle.isVisible = true
             }
 
             override fun onAnimationCancel(p0: Animator) {
@@ -78,10 +78,11 @@ class MainActivity : AppCompatActivity() {
         })
         expand.addListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(p0: Animator) {
+                semiCircle.isVisible = false
             }
 
             override fun onAnimationEnd(p0: Animator) {
-                Log.d("position",  view.translationX.toString())
+
             }
 
             override fun onAnimationCancel(p0: Animator) {
@@ -91,37 +92,21 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
-        expand.duration = 500
-        view.setOnClickListener {
-            expand.start()
-        }
-
-        ivClose.setOnClickListener {
-            fold.start()
-        }
-
-        animShow.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation) {
-//                semiCircle.isVisible = false
-                view.isVisible = true
-
+        expandHalfCircle.addListener(object : Animator.AnimatorListener{
+            override fun onAnimationStart(p0: Animator) {
             }
 
-            override fun onAnimationEnd(animation: Animation) {
-                view.clearAnimation()
+            override fun onAnimationEnd(p0: Animator) {
+                semiCircle.isVisible = false
+                scaleX.start()
+                scaleY.start()
             }
 
-            override fun onAnimationRepeat(animation: Animation) {}
-        })
-        animHide.setAnimationListener(object : Animation.AnimationListener {
-            override fun onAnimationStart(animation: Animation) {}
-            override fun onAnimationEnd(animation: Animation) {
-                view.isInvisible = true
-                ivArrow.startAnimation(arrowAnimation)
-                view.clearAnimation()
+            override fun onAnimationCancel(p0: Animator) {
             }
 
-            override fun onAnimationRepeat(animation: Animation) {}
+            override fun onAnimationRepeat(p0: Animator) {
+            }
         })
 
 
@@ -131,8 +116,8 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onAnimationEnd(animation: Animation) {
-                ivClose.clearAnimation()
-
+                ivArrow.clearAnimation()
+                expandHalfCircle.start()
             }
 
             override fun onAnimationRepeat(animation: Animation) {
