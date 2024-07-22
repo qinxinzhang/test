90c0de640c4f2e9ed5f4ca389133f107ff0c9b07
Qinxin Zhang <44661616+qinxinzhang@users.noreply.github.com>
2024-07-22 21:42:13 +0800

    commit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.test
 
-import android.R.attr.button
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
 import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -10,6 +14,7 @@ import android.view.animation.TranslateAnimation
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.animation.addListener
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 
@@ -30,26 +35,78 @@ class MainActivity : AppCompatActivity() {
         val animShow: Animation = AnimationUtils.loadAnimation(this, R.anim.dialog_in)
         val animHide: Animation = AnimationUtils.loadAnimation(this, R.anim.dialog_out)
 
-        val arrowAnimation = TranslateAnimation(0f,10f,0f,0f)
+        val arrowAnimation = TranslateAnimation(0f, 10f, 0f, 0f)
         arrowAnimation.duration = 500
         arrowAnimation.repeatCount = 6
         arrowAnimation.repeatMode = Animation.REVERSE
 
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenHeight = displayMetrics.heightPixels
+        val screenWidth = displayMetrics.widthPixels
 
-        semiCircle.setOnClickListener {
-            view.startAnimation(animShow)
+        val startPos = resources.getDimension(R.dimen.start_pos)
+        val width = resources.getDimension(R.dimen.view_width)
+        val translation = resources.getDimension(R.dimen.move_x)
 
+
+//        view.setOnClickListener {
+//            val propertyX = PropertyValuesHolder.ofFloat(View.X, 0f)
+//            val propertyY = PropertyValuesHolder.ofFloat(View.Y, 0f)
+//            ObjectAnimator.ofPropertyValuesHolder(view, propertyX, propertyY).apply {
+//                duration = 2000L
+//                start()
+//            }
+//        }
+        val expand = ObjectAnimator.ofFloat(view, "translationX", 0f)
+        val fold = ObjectAnimator.ofFloat(view, "translationX", startPos)
+        fold.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator) {
+
+            }
+
+            override fun onAnimationCancel(p0: Animator) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator) {
+            }
+
+        })
+        expand.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {
+            }
+
+            override fun onAnimationEnd(p0: Animator) {
+                Log.d("position",  view.translationX.toString())
+            }
+
+            override fun onAnimationCancel(p0: Animator) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator) {
+            }
+
+        })
+        expand.duration = 500
+        view.setOnClickListener {
+            expand.start()
         }
+
         ivClose.setOnClickListener {
-            view.startAnimation(animHide)
+            fold.start()
         }
 
         animShow.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation) {
-                semiCircle.isVisible = false
+//                semiCircle.isVisible = false
                 view.isVisible = true
-                        
+
             }
+
             override fun onAnimationEnd(animation: Animation) {
                 view.clearAnimation()
             }
@@ -60,7 +117,6 @@ class MainActivity : AppCompatActivity() {
             override fun onAnimationStart(animation: Animation) {}
             override fun onAnimationEnd(animation: Animation) {
                 view.isInvisible = true
-                semiCircle.isVisible = true
                 ivArrow.startAnimation(arrowAnimation)
                 view.clearAnimation()
             }
